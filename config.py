01a66a9ab9d203d545f0011a2a5c508b36522125
kangle0811 <129728977+kangle0811@users.noreply.github.com>
2025-12-09 14:50:26 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+customhost   = "employee.cw3rlytqjar.us-east-1.rds.amazonaws.com"
+customuser   = "aws_user"
+custompass   = "limHenry0811#"
+customdb     = "employee"
+custombucket = "lowchoonkeat-employee"
+customregion = "us-east-1"
+
+
