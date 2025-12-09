ec00b7fee3ffd013fb9b09f28f1be1df505df148
kangle0811 <129728977+kangle0811@users.noreply.github.com>
2025-12-09 15:06:07 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-customhost   = "employee.cw3rlytqjar.us-east-1.rds.amazonaws.com"
-customuser   = "aws_user"
-custompass   = "limHenry0811#"
-customdb     = "employee"
-custombucket = "lowchoonkeat-employee"
-customregion = "us-east-1"
-
-
